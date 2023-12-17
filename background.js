1b329f6212ce0ea45202d24c113ae138cfaf4996
Gavin John <gavinnjohn@gmail.com>
2023-12-17 15:52:54 -0600

    Fix a few bugs

--- background.js
+++ background.js
@@ -1,11 +1,20 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getCurrentBrandsVersion(){
+  let result = await browser.storage.local.get("brandsVersion");
+  console.log("AmazonBrandFilter: Current brands version is " + result.brandsVersion);
+  return result.brandsVersion
+}
+
 async function checkBrandsVersion(){
   while (true) {
     console.log("AmazonBrandFilter: Checking latest brands list version");
     
-    var currentVersion = await getCurrentBrandsVersion().then(function(version){return version;});
+    var currentVersion = await getCurrentBrandsVersion();
     var latestReleaseUrl = 'https://api.github.com/repos/chris-mosley/AmazonBrandFilterList/releases/latest';
-    var latestRelease = await fetch(versionUrl, {mode: 'cors'})
-      .then(response => response.json())
+    var latestRelease = await fetch(versionUrl, {mode: 'cors'}).then(response => response.json());
     var latestVersion = latestRelease.name;
     var latestTag = latestRelease.tag_name;
     
@@ -23,8 +32,6 @@ async function checkBrandsVersion(){
   }
 }
 
-
-
 async function updateBrandList(tag){
   console.log("AmazonBrandFilter: Starting updateBrandList");
 
@@ -34,44 +41,25 @@ async function updateBrandList(tag){
     .then(text => text.toUpperCase())
     .then(text => text.split("\n"))
     
-
   console.log("AmazonBrandFilter: Brands are " + brandsGet);
   browser.storage.local.set({"brandsList": brandsGet});
-  
 }
 
-async function getCurrentBrandsVersion(){
-
-  await browser.storage.local.get("brandsVersion").then(function(result){
-    console.log("AmazonBrandFilter: Current brands version is " + result.brandsVersion);
-
-    var version = result.brandsVersion;
-    return versionInt})
-  }
-
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function setIcon(){
-  await browser.storage.local.get("abf-enabled").then(function(enabled){
-  if(enabled){
+async function setIcon() {
+  let enabled = await browser.storage.local.get("abf-enabled");
+  if (enabled){
     browser.action.setIcon({
       path: {
         48: "icons/abf-enabled-128.png"
       }
     });
-  }
-  else{
+  } else {
     browser.action.setIcon({
       path: {
         48: "icons/abf-disabled-128.png"
       }
     });
   }
-});
 }
 
 // set the icon the first time the extension is loaded
