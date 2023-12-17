b40f54580aff8c09c696c2fc468afd276bb7de93
Gavin John <gavinnjohn@gmail.com>
2023-12-17 15:49:23 -0600

    Integrate with GitHub releases and Git tags

--- background.js
+++ background.js
@@ -1,43 +1,42 @@
-
-
-
 async function checkBrandsVersion(){
-  while(true){
+  while (true) {
     console.log("AmazonBrandFilter: Checking latest brands list version");
+    
     var currentVersion = await getCurrentBrandsVersion().then(function(version){return version;});
-    var versionUrl = 'https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/main/version.txt'
-    var latestVersion = await fetch(versionUrl, {mode: 'cors'})
-      .then(response => response.text())
-      .then(text => parseInt(text))
+    var latestReleaseUrl = 'https://api.github.com/repos/chris-mosley/AmazonBrandFilterList/releases/latest';
+    var latestRelease = await fetch(versionUrl, {mode: 'cors'})
+      .then(response => response.json())
+    var latestVersion = latestRelease.name;
+    var latestTag = latestRelease.tag_name;
     
-      console.log("AmazonBrandFilter: Latest brands list version is " + latestVersion);
-      console.log("AmazonBrandFilter: Current brands list version is " + currentVersion);
-    if(currentVersion <= latestVersion || currentVersion == undefined){
-      
+    console.log("AmazonBrandFilter: Latest brands list version is " + latestVersion);
+    console.log("AmazonBrandFilter: Current brands list version is " + currentVersion);
+    
+    if (currentVersion != latestVersion){
         console.log("AmazonBrandFilter: Downloading latest brands list");
-        downloadLatestBrands()
+        updateBrandList(latestTag);
         browser.storage.local.set({"brandsVersion": latestVersion});
-      }
+    }
     
-      console.log("AmazonBrandFilter: backgroup.js sleeping for one day");
+    console.log("AmazonBrandFilter: backgroup.js sleeping for one day");
     await sleep(86400000);
   }
 }
 
 
 
-async function downloadLatestBrands(){
-  console.log("AmazonBrandFilter: Starting downloadLatestBrands");
+async function updateBrandList(tag){
+  console.log("AmazonBrandFilter: Starting updateBrandList");
 
-  var brandsUrl = 'https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/main/brands.txt'
+  var brandsUrl = `https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/${tag}/brands.txt`
   var brandsGet = await fetch(brandsUrl, {mode: 'cors'})
     .then(response => response.text())
     .then(text => text.toUpperCase())
     .then(text => text.split("\n"))
     
 
-    console.log("AmazonBrandFilter: Brands are " + brandsGet);
-    browser.storage.local.set({"brandsList": brandsGet});
+  console.log("AmazonBrandFilter: Brands are " + brandsGet);
+  browser.storage.local.set({"brandsList": brandsGet});
   
 }
 
@@ -46,7 +45,7 @@ async function getCurrentBrandsVersion(){
   await browser.storage.local.get("brandsVersion").then(function(result){
     console.log("AmazonBrandFilter: Current brands version is " + result.brandsVersion);
 
-    var versionInt = parseInt(result.brandsVersion);
+    var version = result.brandsVersion;
     return versionInt})
   }
 
@@ -77,5 +76,5 @@ async function setIcon(){
 
 // set the icon the first time the extension is loaded
 setIcon();
-// downloadLatestBrands();
+// Start checking for updates
 checkBrandsVersion();
