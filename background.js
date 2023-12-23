836218f1e1949acd9e1e66e117ececbf1e1beff3
Artini04 <89753669+Artini04@users.noreply.github.com>
2023-12-22 21:06:54 -0700

    completely refactor background.js

--- background.js
+++ background.js
@@ -2,23 +2,26 @@
 //      BACKGROUND START        //
 // ============================ //
 // Check if this is the first run!
-if (!(await getFirstRun())) {
-  console.log("AmazonBrandFilter: %cFirst run, setting defaults!", "color: yellow");
+if (await getFirstRun()) {
+  console.log(
+    "AmazonBrandFilter: %cFirst run, setting defaults!",
+    "color: yellow"
+  );
 
-  browser.storage.local.set({ enabled: false });
+  browser.storage.local.set({ enabled: true });
   browser.storage.local.set({ brandsVersion: 0 });
   browser.storage.local.set({ brandsCount: 0 });
   browser.storage.local.set({ brandsMap: {} });
   browser.storage.local.set({ refinerBypass: true });
   browser.storage.local.set({ abfFirstRun: false });
 } else {
-  console.log("AmazonBrandFilter: Not first run!");
+  console.log("AmazonBrandFilter: %cNot first run!", "color: yellow");
 }
 
 // Set the icon the first time the extension is loaded
 setIcon();
-// Check for updates in the background once everyday
-checkBrandsListVersion()
+// Periodically check for updates once everyday
+checkBrandsListVersion();
 
 // ========================== //
 //      INIT FUNCTIONS        //
@@ -48,7 +51,7 @@ async function setIcon() {
   }
 }
 
-async function checkBrandsListVersion(callDelay = 8.64e+7) {
+async function checkBrandsListVersion(callDelay = 8.64e7) {
   console.log("AmazonBrandFilter: Checking for updates!");
 
   const brandLatestReleaseUrl = "https://api.github.com/repos/chris-mosley/AmazonBrandFilterList/releases/latest";
@@ -62,29 +65,33 @@ async function checkBrandsListVersion(callDelay = 8.64e+7) {
 
   const latestBrandsVersion = parseInt(latestBrandRelease.tag_name.slice(1));
 
-  let brandsMap = {};
   if (currentBrandsVersion != latestBrandsVersion) {
     console.log(
       "AmazonBrandFilter: %cCurrent version does not match the latest version!",
       "color: lightcoral",
-      `\nAmazonBrandFilter: Current: ${currentBrandsVersion} Latest: ${latestBrandsVersion}`
+      `\nAmazonBrandFilter: Current: ${currentBrandsVersion} | Latest: ${latestBrandsVersion}`
     );
 
     // Set the latest version as current then download the latest brand list
     browser.storage.local.set({ brandsVersion: latestBrandsVersion });
-    brandsMap = await downloadBrandList();
-  } else {
-    console.log("AmazonBrandFilter: %cCurrent version match the latest version!", "color: lightgreen");
 
-    // Load the brand list from the local storage
-    brandsMap = { brandsMap } = await browser.storage.local.get("brandsMap");
+    // Download the latest brands list
+    downloadBrandList();
+  } else {
+    console.log(
+      "AmazonBrandFilter: %cCurrent version match the latest version!",
+      "color: lightgreen",
+      `\nAmazonBrandFilter: Current: ${currentBrandsVersion} | Latest: ${latestBrandsVersion}`
+    );
   }
 
   // This is similar to the sleep() function you made. It simplifies the
   // logic needed and removes the while() loop and also no, this isn't
-  // recursion or at least they call it  "pseudo-recursion" which doesn't 
+  // recursion or at least they call it "pseudo-recursion" which doesn't
   // add anything to the stack.
-  console.log("AmazonBrandFilter:\n[Background.js] Sleeping for one day!")
+  //
+  // https://stackoverflow.com/questions/6685396/execute-the-setinterval-function-without-delay-the-first-time
+  console.log(`AmazonBrandFilter: [Background.js] Sleeping!`);
   setTimeout(checkBrandsListVersion, callDelay);
 }
 
@@ -98,7 +105,11 @@ async function getCurrentBrandsVersion() {
 }
 
 async function downloadBrandList() {
-  console.log("AmazonBrandFilter: %cDownloading new release!", "color: lightgreen");
+  console.log(
+    "AmazonBrandFilter: %cDownloading new release!",
+    "color: lightgreen"
+  );
+
   const brandsListUrl = "https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/main/brands.txt";
 
   // Fetch the brand list AS A TEXT FILE!
@@ -109,13 +120,17 @@ async function downloadBrandList() {
       console.error(e, "AmazonBrandFilter: Failed downloading brands list!");
     });
 
+  // Initialize variables
   let maxWordCount = 0,
-    brandsListMap = {},
-    brandsCount = 0;
+    brandsListMap = {};
+
+  // Set brands list count
+  const brandsCount = brandsListFetch.length
 
   // Build the brands list map
   for (const brandName of brandsListFetch) {
     console.debug(`AmazonBrandFilter: Adding ${brandName} to brands list!`);
+
     // Append key-value to brand list map
     brandsListMap[brandName] = true;
 
@@ -123,10 +138,6 @@ async function downloadBrandList() {
     if (brandName.split(" ").length > maxWordCount) {
       maxWordCount = brandName.split(" ").length;
     }
-
-    // brandsListMap.length returns undefined
-    // Add 1 to brandsCount for every brand
-    brandsCount += 1;
   }
 
   // Save items to local storage
@@ -138,6 +149,4 @@ async function downloadBrandList() {
   console.log(`AmazonBrandFilter: Brands count is ${brandsCount}!`);
   console.log(`AmazonBrandFilter: Max brand word count is ${maxWordCount}!`);
   console.log("AmazonBrandFilter: Showing brand list:", brandsListMap);
-
-  return brandsListMap;
 }
