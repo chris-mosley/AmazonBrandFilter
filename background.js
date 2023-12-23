eaa2e978b3b0a54c013b6edb2488863c7900eb8f
Artini04 <89753669+Artini04@users.noreply.github.com>
2023-12-22 20:16:06 -0700

    Update background.js

--- background.js
+++ background.js
@@ -3,9 +3,9 @@
 // ============================ //
 // Check if this is the first run!
 if (!(await getFirstRun())) {
-  console.log("AmazonBrandFilter: First run, setting defaults!");
+  console.log("AmazonBrandFilter: %cFirst run, setting defaults!", "color: yellow");
 
-  browser.storage.local.set({ enabled: true });
+  browser.storage.local.set({ enabled: false });
   browser.storage.local.set({ brandsVersion: 0 });
   browser.storage.local.set({ brandsCount: 0 });
   browser.storage.local.set({ brandsMap: {} });
@@ -15,24 +15,44 @@ if (!(await getFirstRun())) {
   console.log("AmazonBrandFilter: Not first run!");
 }
 
-// set the icon the first time the extension is loaded
+// Set the icon the first time the extension is loaded
 setIcon();
-// Check for updates!
-checkBrandsListVersion();
+// Check for updates in the background once everyday
+checkBrandsListVersion()
 
 // ========================== //
 //      INIT FUNCTIONS        //
 // ========================== //
 
-async function getFirstRun(){
-  const { abfFirstRun } = await browser.storage.local.get("abfFirstRun")
-  console.debug(`AmazonBrandFilter: First run status is ${abfFirstRun}`)
-  return abfFirstRun ? true : false
+async function getFirstRun() {
+  const { abfFirstRun } = await browser.storage.local.get("abfFirstRun");
+  console.debug(`AmazonBrandFilter: First run status is ${abfFirstRun}`);
+  return abfFirstRun ? true : false;
 }
 
-async function checkBrandsListVersion() {
+async function setIcon() {
+  const { enabled } = await browser.storage.local.get("enabled");
+
+  if (enabled) {
+    browser.action.setIcon({
+      path: {
+        128: "icons/abf-enabled-128.png",
+      },
+    });
+  } else {
+    browser.action.setIcon({
+      path: {
+        128: "icons/abf-disabled-128.png",
+      },
+    });
+  }
+}
+
+async function checkBrandsListVersion(callDelay = 8.64e+7) {
+  console.log("AmazonBrandFilter: Checking for updates!");
+
   const brandLatestReleaseUrl = "https://api.github.com/repos/chris-mosley/AmazonBrandFilterList/releases/latest";
-  const { brandsVersion: currentBrandVersion } = await browser.storage.local.get("brandsVersion") ?? { brandsVersion: 0 }
+  const currentBrandsVersion = await getCurrentBrandsVersion();
 
   const latestBrandRelease = await fetch(brandLatestReleaseUrl)
     .then((r) => r.json())
@@ -43,14 +63,38 @@ async function checkBrandsListVersion() {
   const latestBrandsVersion = parseInt(latestBrandRelease.tag_name.slice(1));
 
   let brandsMap = {};
-  if (currentBrandVersion != latestBrandsVersion) {
-    console.log("AmazonBrandFilter: %cCurrent version does not match the latest version!", "color: lightcoral", `\nAmazonBrandFilter: Current: ${currentBrandVersion} Latest: ${latestBrandsVersion}`)
+  if (currentBrandsVersion != latestBrandsVersion) {
+    console.log(
+      "AmazonBrandFilter: %cCurrent version does not match the latest version!",
+      "color: lightcoral",
+      `\nAmazonBrandFilter: Current: ${currentBrandsVersion} Latest: ${latestBrandsVersion}`
+    );
+
+    // Set the latest version as current then download the latest brand list
     browser.storage.local.set({ brandsVersion: latestBrandsVersion });
-    brandsMap = await downloadBrandList()
+    brandsMap = await downloadBrandList();
   } else {
-    console.log("AmazonBrandFilter: %cCurrent version match the latest version!", "color: lightgreen")
-    brandsMap = { brandsMap } = await browser.storage.local.get("brandsMap")
+    console.log("AmazonBrandFilter: %cCurrent version match the latest version!", "color: lightgreen");
+
+    // Load the brand list from the local storage
+    brandsMap = { brandsMap } = await browser.storage.local.get("brandsMap");
   }
+
+  // This is similar to the sleep() function you made. It simplifies the
+  // logic needed and removes the while() loop and also no, this isn't
+  // recursion or at least they call it  "pseudo-recursion" which doesn't 
+  // add anything to the stack.
+  console.log("AmazonBrandFilter:\n[Background.js] Sleeping for one day!")
+  setTimeout(checkBrandsListVersion, callDelay);
+}
+
+// ============================ //
+//      HELPER FUNCTIONS        //
+// ============================ //
+
+async function getCurrentBrandsVersion() {
+  const { brandsVersion } = await browser.storage.local.get("brandsVersion");
+  return brandsVersion ?? 0;
 }
 
 async function downloadBrandList() {
@@ -65,7 +109,9 @@ async function downloadBrandList() {
       console.error(e, "AmazonBrandFilter: Failed downloading brands list!");
     });
 
-  let maxWordCount = 0, brandsListMap = {}, brandsCount = 0;
+  let maxWordCount = 0,
+    brandsListMap = {},
+    brandsCount = 0;
 
   // Build the brands list map
   for (const brandName of brandsListFetch) {
@@ -80,7 +126,7 @@ async function downloadBrandList() {
 
     // brandsListMap.length returns undefined
     // Add 1 to brandsCount for every brand
-    brandsCount += 1
+    brandsCount += 1;
   }
 
   // Save items to local storage
@@ -89,159 +135,9 @@ async function downloadBrandList() {
   browser.storage.local.set({ brandsCount: brandsCount });
 
   // Console logs
-  console.log(`AmazonBrandFilter: Brands count is ${brandsCount}`);
-  console.log(`AmazonBrandFilter: Max brand word count is ${maxWordCount}`);
-  console.log("AmazonBrandFilter: Brand list!", brandsListMap);
+  console.log(`AmazonBrandFilter: Brands count is ${brandsCount}!`);
+  console.log(`AmazonBrandFilter: Max brand word count is ${maxWordCount}!`);
+  console.log("AmazonBrandFilter: Showing brand list:", brandsListMap);
 
   return brandsListMap;
 }
-
-async function processBrandList() {
-
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function getCurrentBrandsVersion(){
-  let mapStatus = await browser.storage.local.get("brandsMap");
-  if(mapStatus.brandsMap == undefined){
-    // if the map is undefined lets make sure to update regardless of version
-    return 0;
-  }
-  let result = await browser.storage.local.get("brandsVersion");
-  console.log("AmazonBrandFilter: Current brands version is " + result.brandsVersion);
-  return result.brandsVersion
-}
-
-async function checkBrandsVersionOld(){
-  var _ = browser.storage.local.get("")
-  while (true) {
-    console.log("AmazonBrandFilter: Checking latest brands list version");
-    // var currentVersion = await getCurrentBrandsVersion();
-    var currentVersion = 0;
-    var latestReleaseUrl = 'https://api.github.com/repos/chris-mosley/AmazonBrandFilterList/releases/latest';
-    var latestRelease = await fetch(latestReleaseUrl, {mode: 'cors'}).then(response => response.json());
-    var latestVersion = parseInt(latestRelease.tag_name.slice(1));
-    
-    console.log("AmazonBrandFilter: Latest brands list version is " + latestVersion);
-    console.log("AmazonBrandFilter: Current brands list version is " + currentVersion);
-    
-    if (currentVersion != latestVersion){
-        console.log("AmazonBrandFilter: Downloading latest brands list");
-        try{
-          // updateBrandList();
-          updateBrandMap();
-        }
-        catch(err){
-          console.error("AmazonBrandFilter: Error downloading brands list");
-          return;
-        }
-        browser.storage.local.set({"brandsVersion": latestVersion});
-    }
-    
-    console.log("AmazonBrandFilter: background.js sleeping for one day");
-    await sleep(86400000);
-  }
-}
-
-async function updateBrandList(){
-  console.log("AmazonBrandFilter: Starting updateBrandList");
-
-  var brandsUrl = "https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/main/brands.txt"
-
-  try{
-    var brandsGet = await fetch(brandsUrl, {mode: 'cors'})
-      .then(response => response.text())
-      .then(text => text.toUpperCase())
-      .then(text => text.split("\n"))
-  }
-  catch(err){
-    console.error("AmazonBrandFilter: Error downloading brands list");
-    return;
-  }
-
-  browser.storage.local.set({"brandsCount": brandsGet.length});
-  console.log("AmazonBrandFilter: Brands count is " + brandsGet.length);
-  console.log("AmazonBrandFilter: Brands are " + brandsGet);
-  
-  browser.storage.local.set({"brandsList": brandsGet});
-}
-
-async function updateBrandMap(){
-  console.log("AmazonBrandFilter: Starting updateBrandList");
-
-  var brandsUrl = "https://raw.githubusercontent.com/chris-mosley/AmazonBrandFilterList/main/brands.txt"
-
-  try{
-    var brandsGet = await fetch(brandsUrl, {mode: 'cors'})
-      .then(response => response.text())
-      .then(text => text.toUpperCase())
-      .then(text => text.split("\n"))
-  }
-  catch(err){
-    console.error("AmazonBrandFilter: Error downloading brands list");
-    return;
-  }
-  var brandsMap = {};
-  for(var i=0; i <brandsGet.length; i++)
-  {
-    console.debug("AmazonBrandFilter: Adding " + brandsGet[i] + " to brands list");
-      // protect against possible empty lines in the list
-    if(brandsGet[i] != "")
-      {
-        brandsMap[brandsGet[i]] = true;
-      }
-  }
-  
-  
-  console.log("AmazonBrandFilter: Brands count is " + brandsGet.length);
-  
-  
-  
-  browser.storage.local.set({"brandsMap": brandsMap});
-
-  let keys = Object.keys(brandsMap);
-  var maxWordCount = 0;
-  for(var i=0; i < keys.length; i++){
-    if(keys[i].split(" ").length > maxWordCount){
-      maxWordCount = keys[i].split(" ").length;
-    }
-  }
-  browser.storage.local.set({"maxWordCount": maxWordCount});
-  console.log("AmazonBrandFilter: Max brand word count is " + maxWordCount);
-  console.log("AmazonBrandFilter: Brands are " + keys);
-  browser.storage.local.set({"brandsCount": keys.length});
-  
-  console.log("%cBrand map", "color: green", brandsMap)
-}
-
-
-// function createBrandMap(wordList, depth=0){
-//   if(depth == wordList.length - 1){
-//     return true;
-//   }
-//   var brandMap = {};
-//   brandMap[wordList[depth+1]] = createBrandMap(wordList, depth + 1);
-//   return brandMap;
-// }
-
-async function setIcon() {
-  let enabled = await browser.storage.local.get("abf-enabled");
-  if (enabled){
-    browser.action.setIcon({
-      path: {
-        48: "icons/abf-enabled-128.png"
-      }
-    });
-  } else {
-    browser.action.setIcon({
-      path: {
-        48: "icons/abf-disabled-128.png"
-      }
-    });
-  }
-}
-
-
