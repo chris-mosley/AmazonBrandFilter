14b30415a4a8005a2806a7d96b774a5c2cb8ba51
Neil Murphy <barrymun@tcd.ie>
2024-01-05 07:56:33 +0000

    define the content_scripts only once so we do not have to update it in multiple places, expanding on the lint-staged rules in package.json for more standardised code, remove include_globs key from content_scripts

--- scripts/post-build-update-manifest.js
+++ scripts/post-build-update-manifest.js
@@ -1,18 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // read the version from package.json
-const packageJson = require('../package.json');
+const packageJson = require("../package.json");
 const manifestFilePath = path.join(__dirname, `../dist/manifest.json`);
+const contentScriptsJson = require("../engines/common/content-scripts.json");
 
 try {
   // read the manifest file
-  const manifest = JSON.parse(fs.readFileSync(manifestFilePath));
+  let manifest = JSON.parse(fs.readFileSync(manifestFilePath));
 
   // update the relevant keys
   manifest.version = packageJson.version;
   manifest.name = packageJson.name;
   manifest.description = packageJson.description;
+  manifest = { ...manifest, ...contentScriptsJson };
 
   // write the updated manifest file
   fs.writeFileSync(manifestFilePath, JSON.stringify(manifest, null, 2));
